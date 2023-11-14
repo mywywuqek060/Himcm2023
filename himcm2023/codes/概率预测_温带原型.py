0ba8dba2741c192b01efb479976ef8ec4d7a2aa5
mywywuqek060 <isow5122@foxmail.com>
2023-11-14 22:12:15 +0800

    qwq

--- himcm2023/codes/概率预测_温带原型.py
+++ himcm2023/codes/概率预测_温带原型.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #变量表
-r = 0.05  # 内在增长率
-K = 1000  # 环境承载上限
-S_p = 150  # 每一颗蒲公英每个月平均产生的种子数量
-G_s = 0.01  # 发芽率
-alpha = 0.05  # 植物竞争对增长率的影响
-beta = 0.05  # 动物竞争对增长率的影响
-Ai = 1.2  # 动物的恒定正面影响
-Cf_temperate = 1.0  # 气候因子（温带为1.0) 越高越好 
-seasonal_factors = [1.2, 1.2, 1.0, 1.0, 1.0, 0.8, 0.8, 0.8, 0.3, 0.3, 0.3, 1.2]  
+r = 0.08  # 内在增长率
+K = 800  # 环境承载上限
+S_p = 3125  # 每一颗蒲公英每个月平均产生的种子数量
+G_s = 0.0003  # 发芽率
+alpha = 0.07  # 植物竞争对增长率的影响
+beta = 0.07  # 动物竞争对增长率的影响
+Ai = 1.3  # 动物的恒定正面影响
+Cf = 1.2  # 气候因子（温带为1.0) 越高越好 
+seasonal_factors = [1.1, 1.1, 1.1, 1.1, 1.0, 1.0, 1.0, 1.0, 1.1, 1.1, 1.1, 1.1]  
 
 # 定义湿度和竞争特性
 humidity_distribution = {'L': 0.2, 'M': 0.6, 'H': 0.2}  # 出现低中高湿度的概率
@@ -39,10 +39,10 @@ for i in range(num_simulations):
     population = [initial_population]
     for month, Sf in zip(months[:-1], seasonal_factors):
         next_population = calculate_population_probabilistic(
-            population[-1], r, K, humidity_distribution, Cf_temperate,
+            population[-1], r, K, humidity_distribution, Cf,
             competition_distribution, G_s, Ai, alpha, beta, S_p, Sf
         )
-        # 确保种群大小不小于零
+        # 确保种群大小不小于0
         next_population = max(next_population, 0)
         population.append(next_population)
     monthly_populations[i, :] = population
@@ -54,13 +54,31 @@ percentile_90 = np.percentile(monthly_populations, [5, 95], axis=0)
 
 # 使用不同蓝色的阴影绘制置信区间
 plt.figure(figsize=(14, 8))
-plt.fill_between(months, percentile_90[0, :], percentile_90[1, :], color='lightblue', alpha=0.5, label='90% Confidence Interval')
-plt.fill_between(months, percentile_75[0, :], percentile_75[1, :], color='mediumblue', alpha=0.5, label='75% Confidence Interval')
-plt.fill_between(months, percentile_50[0, :], percentile_50[1, :], color='darkblue', alpha=0.5, label='50% Confidence Interval')
+plt.fill_between(months, percentile_90[0, :], percentile_90[1, :], color='lightblue', alpha=0.5, label='90% Confidence Interval (CI)')
+plt.fill_between(months, percentile_75[0, :], percentile_75[1, :], color='mediumblue', alpha=0.5, label='75% Confidence Interval (CI)')
+plt.fill_between(months, percentile_50[0, :], percentile_50[1, :], color='darkblue', alpha=0.5, label='50% Confidence Interval (CI)')
 plt.plot(months, np.median(monthly_populations, axis=0), color='navy', label='Median Population')
-plt.title('Confidence Intervals for Dandelion Population Spread')
+plt.title('Confidence Intervals for Dandelion Population Spread: Tropical')
 plt.xlabel('Months')
 plt.ylabel('Dandelion Population')
 plt.legend(loc='upper left')
 plt.grid(True)
 plt.show()
+
+# Calculate confidence intervals for each month
+confidence_intervals = {
+    'Month': months,
+    'Median': np.median(monthly_populations, axis=0),
+    '90% Lower': percentile_90[0, :],
+    '90% Upper': percentile_90[1, :],
+    '75% Lower': percentile_75[0, :],
+    '75% Upper': percentile_75[1, :],
+    '50% Lower': percentile_50[0, :],
+    '50% Upper': percentile_50[1, :],
+}
+
+# Print the results for each month
+print("Month\tMedian\t90% Lower\t90% Upper\t75% Lower\t75% Upper\t50% Lower\t50% Upper")
+for month in months:
+    print(f"{int(month)}\t{confidence_intervals['Median'][int(month)]:.2f}\t{confidence_intervals['90% Lower'][int(month)]:.2f}\t{confidence_intervals['90% Upper'][int(month)]:.2f}\t{confidence_intervals['75% Lower'][int(month)]:.2f}\t{confidence_intervals['75% Upper'][int(month)]:.2f}\t{confidence_intervals['50% Lower'][int(month)]:.2f}\t{confidence_intervals['50% Upper'][int(month)]:.2f}")
+
